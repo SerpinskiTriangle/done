5ad3262afe98c132d9b422e355e43b054b9ad5df
SerpinskiTriangle <kolesov.id@gmail.com>
2025-03-23 21:40:48 -0400

    trivial file io added, gonna get distracted by lambda calculus

--- zipf/zipf.c
+++ zipf/zipf.c
@@ -1,3 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+
 int main(){
+  FILE *file = fopen("test.txt", "r");
+
+
+  if (file == NULL){
+    printf("failed to open file \n");
+    exit(EXIT_FAILURE);
+  }
+
+
+  char c;
+  while ((c = fgetc(file)) != EOF){
+    printf("%c", c);
+  }
+
+
   return 0;
 }
