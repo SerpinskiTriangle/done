d078697971e835c0523b58b7b6f00297b9db64f6
SerpinskiTriangle <kolesov.id@gmail.com>
2025-03-27 20:10:40 -0400

    worked first try i might be the goat

--- zipf/zipf.c
+++ zipf/zipf.c
@@ -16,12 +16,14 @@ struct word_node {
 
 
 
-char word_terminators[3] = [' ', '\n', '\t'];
+char word_terminators[3] = {' ', '\n', '\t'};
 
 
-int char_in(char char, char *string, int string_len){
+int char_in(char ch, char *string, int string_len){
   for (int i = 0; i < string_len; i++){
-    char == string[i] ? return 1 : 0;
+    if (ch == string[i]){
+      return 1;
+    }
   }
   return -1;
 }
@@ -44,9 +46,35 @@ int same_word(char *word_1, int word_1_len, char *word_2, int word_2_len){
 
 int insert_word(char *word, int word_len, struct word_node *word_list, int word_list_len){
   for (int index = 0; index < word_list_len; index++){
-     
+    if (same_word(word_list[index].word, word_list[index].word_length, word, word_len)){
+      word_list[index].occurances++;
+      return 0;
+    }
+
+
+    if (word_list[index].word == NULL){
+      // if it's a new word
+      word_list[index].word = (char *)malloc(word_len * sizeof(char));
+      memcpy(word_list[index].word, word,    word_len * sizeof(char));
+
+      word_list[index].word_length = word_len;
+      word_list[index].occurances = 1;
+      return 0;
+    }
+  }
+
+  printf("mm");
+  exit(EXIT_FAILURE);
+}
+
+
+void print_word(char *word, int word_len){
+  for (int index = 0; index < word_len; index++){
+    printf("%c", word[index]);
   }
 }
+
+
 
 
 int main(){
@@ -63,17 +91,30 @@ int main(){
 
   char *word = (char *)calloc(MAX_WORD_SIZE, sizeof(char));
 
-  struct word_node *word_list = (struct word_node *)calloc(MAX_WORD_COUNT, sizeof(word_node));
+  struct word_node *word_list = (struct word_node *)calloc(MAX_WORD_COUNT, sizeof(struct word_node));
+
+  int current_word_length = 0;
+
+  int stored_words = 0;
 
   while ((c = fgetc(file)) != EOF){
     //check for word terminating chars
-    if(char_in(c, word_terminators)){
-           
+    if(char_in(c, word_terminators, 3) == 1){
+      insert_word(word, current_word_length, word_list, MAX_WORD_COUNT);
+      current_word_length = 0;
+      stored_words++;
+      continue;
     }
-      //if not add char to word buffer
-      //if so handle the word
+
+
+    word[current_word_length] = c;
+    current_word_length++;
   }
 
 
+  for (int index = 0; index < stored_words; index++){
+    print_word(word_list[index].word, word_list[index].word_length);
+    printf(" occurances: %d\n", word_list[index].occurances);
+  }
   return 0;
 }
